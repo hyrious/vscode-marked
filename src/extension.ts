32f7ccf33f04c4ed8d2d16cb079699356e5e0dbc
hyrious <hyrious@outlook.com>
2022-06-28 10:45:56 +0800

    hack: support images

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 import * as vscode from "vscode";
+import * as uri from "vscode-uri";
 
 import html from "@hyrious/marked-cli/index.vscode.html";
+
+declare function setTimeout(fn: () => void, ms: number): any;
 
 interface Item {
   panel: vscode.WebviewPanel;
   doc: vscode.TextDocument;
   update: () => void;
   close: () => void;
+  asWebviewUri: (uri: vscode.Uri) => vscode.Uri;
 }
 
 let panels = new Set<Item>();
@@ -19,15 +23,27 @@ function findPanelByDoc(doc: vscode.TextDocument): Item | undefined {
   }
 }
 
-function createPanel(doc: vscode.TextDocument): Item {
+function createPanel(doc: vscode.TextDocument, subscriptions: { dispose(): any }[]): Item {
+  const baseRoots: vscode.Uri[] = [];
+  const folder = vscode.workspace.getWorkspaceFolder(doc.uri);
+  if (folder) {
+    const workspaceRoots = vscode.workspace.workspaceFolders?.map(folder => folder.uri);
+    if (workspaceRoots) {
+      baseRoots.push(...workspaceRoots);
+    }
+  } else {
+    baseRoots.push(uri.Utils.dirname(doc.uri));
+  }
+
   const panel = vscode.window.createWebviewPanel(
     "vscode-marked.panel",
     "Preview Markdown",
     vscode.ViewColumn.Beside,
-    { enableScripts: true }
+    { enableScripts: true, enableForms: false, localResourceRoots: baseRoots }
   );
 
   function update() {
+    panel.webview.postMessage(JSON.stringify({ notifyTransformSrc: true }));
     panel.webview.postMessage(JSON.stringify(doc.getText()));
   }
 
@@ -36,13 +52,46 @@ function createPanel(doc: vscode.TextDocument): Item {
     panels.delete(item);
   }
 
-  const item: Item = { panel, doc, update, close };
+  function asWebviewUri(resource: vscode.Uri) {
+    return panel.webview.asWebviewUri(resource);
+  }
+
+  const item: Item = { panel, doc, update, close, asWebviewUri };
   panels.add(item);
 
-  panel.onDidDispose(close);
   panel.webview.html = html;
 
+  type MarkedMessage = { transformSrc: string[] };
+  function transformSrc(src: string) {
+    let ret = "";
+    let uri = vscode.Uri.parse("markdown-link:" + src);
+    if (folder) {
+      uri = vscode.Uri.joinPath(folder.uri, uri.fsPath);
+      ret = asWebviewUri(uri).toString(true);
+    } else {
+      ret = uri.toString(true).replace(/^markdown-link:/, "");
+    }
+    return ret;
+  }
+
+  panel.webview.onDidReceiveMessage(
+    (ev: MarkedMessage) => {
+      if (ev.transformSrc) {
+        panel.webview.postMessage(
+          JSON.stringify({
+            transformSrc: ev.transformSrc.map(src => [src, transformSrc(src)]),
+          })
+        );
+      }
+    },
+    undefined,
+    subscriptions
+  );
+
+  panel.onDidDispose(close, undefined, subscriptions);
+
   update();
+  setTimeout(update, 1000);
 
   return item;
 }
@@ -59,7 +108,7 @@ export function activate(context: vscode.ExtensionContext) {
       if ((item = findPanelByDoc(doc))) {
         item.panel.reveal();
       } else {
-        createPanel(doc);
+        createPanel(doc, context.subscriptions);
       }
     })
   );
